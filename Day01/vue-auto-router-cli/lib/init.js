c03ffbde697014f5c36e01b9b9620baca85f0a9a
chrysanthemum-boy <1447051860@qq.com>
2021-09-21 03:13:55 +0800

    更新字体

--- Day01/vue-auto-router-cli/lib/init.js
+++ Day01/vue-auto-router-cli/lib/init.js
@@ -3,11 +3,24 @@ const {promisify}  = require("util");
 const figlet  = promisify(require("figlet"));
 const chalk  = require("chalk");
 const clear  = require("clear");
-const log = content => console.log(chalk.green(content))
+const log = content => console.log(chalk.red(content))
 
 module.exports = async name =>{
     // 打印欢迎界面
     clear()
-    const data = await figlet("Welcome Fann")
-    log(data)
+    figlet.text('Welcome Fann', {
+        font: 'Ghost',
+        color: 'red',
+        //horizontalLayout: 'default',
+        //verticalLayout: 'default',
+        //width: 80,
+        whitespaceBreak: true
+    }, function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(data);
+    });
 }
